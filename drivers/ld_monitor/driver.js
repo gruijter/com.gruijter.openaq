de5a43c3d4994fb024c36873c8637878e158a059
gruijter <gruijter@hotmail.com>
2023-01-24 11:40:50 +0100

    v2.2.2 Fix Luftdaten yet another API change. Removed LD@homey device during pair.

--- drivers/ld_monitor/driver.js
+++ drivers/ld_monitor/driver.js
@@ -56,12 +56,20 @@ class LDMonitorDriver extends GenericAQMonitorDriver {
 					hostname: 'data.sensor.community', // old: 'api.luftdaten.info'
 					path: `/airrohr/v1/filter/area=${settings.lat},${settings.lon},${dst}`,
 					headers: {
-						'Content-Length': 0,
-						'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+						// 'Content-Length': 0,
+						'Cache-Control': 'no-cache',
+						Host: 'data.sensor.community',
+						Origin: 'https://maps.sensor.community',
+						Referer: 'https://maps.sensor.community/',
+						'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+						Accept: '*/*',
+						Connection: 'keep-alive',
 					},
 					method: 'GET',
 				};
+				// console.log(options);
 				const result = await this._makeHttpsRequest(options);
+				// console.log(result.statusCode, result.body);
 				if (result.statusCode !== 200 || !result.headers['content-type'].includes('application/json')) {
 					throw Error('Wrong content type, expected application/json');
 				}
@@ -120,20 +128,20 @@ class LDMonitorDriver extends GenericAQMonitorDriver {
 		this.discover = async (settings) => {
 			try {
 				const deviceSets = [
-					{
-						name: 'LD Monitor @Homey',
-						includeIndoor: false,
-						pollingInterval: 1,
-						dst: settings.dst,
-						capabilities,
-					},
-					{
-						name: 'LD Monitor @Homey_with_indoor',
-						includeIndoor: true,
-						pollingInterval: 1,
-						dst: settings.dst,
-						capabilities,
-					},
+					// {
+					// 	name: 'LD Monitor @Homey',
+					// 	includeIndoor: false,
+					// 	pollingInterval: 1,
+					// 	dst: settings.dst,
+					// 	capabilities,
+					// },
+					// {
+					// 	name: 'LD Monitor @Homey_with_indoor',
+					// 	includeIndoor: true,
+					// 	pollingInterval: 1,
+					// 	dst: settings.dst,
+					// 	capabilities,
+					// },
 				];
 				const results = await this.getRawData({ settings });
 				const filtered = results
@@ -194,100 +202,100 @@ Alle Sensorenwerte der letzten 5 Minuten -> http://api.luftdaten.info/static/v1/
 Werte der letzten 5 Minuten eines bestimmten Sensors -> http://api.luftdaten.info/v1/sensor/sensorid/
 
 [ { id: 5935220196,
-    location:
-     { country: 'BE',
-       id: 15177,
-       indoor: 0,
-       longitude: '4.412',
-       altitude: '72.8',
-       exact_location: 0,
-       latitude: '50.844' },
-    sensordatavalues:
-     [ { id: 12607161900, value: '29.57', value_type: 'P1' },
-       { value: '16.87', id: 12607161901, value_type: 'P2' },
-       [length]: 2 ],
-    sensor:
-     { id: 27796,
-       pin: '1',
-       sensor_type: { manufacturer: 'Nova Fitness', name: 'SDS011', id: 14 } },
-    sampling_rate: null,
-    timestamp: '2020-01-01 10:57:01' },
-  { location:
-     { latitude: '51.842',
-       exact_location: 0,
-       id: 11774,
-       country: 'NL',
-       altitude: '30.9',
-       indoor: 0,
-       longitude: '5.858' },
-    id: 5935220194,
-    sensordatavalues:
-     [ { value_type: 'temperature', id: 12607161896, value: '2.40' },
-       { value_type: 'humidity', id: 12607161897, value: '99.90' },
-       [length]: 2 ],
-    sensor:
-     { id: 23210,
-       pin: '7',
-       sensor_type: { name: 'DHT22', id: 9, manufacturer: 'various' } },
-    timestamp: '2020-01-01 10:57:01',
-    sampling_rate: null },
-  { sensordatavalues:
-     [ { value_type: 'temperature', id: 12607161885, value: '10.30' },
-       { value_type: 'humidity', value: '94.80', id: 12607161886 },
-       [length]: 2 ],
-    location:
-     { latitude: '50.796',
-       exact_location: 0,
-       country: 'DE',
-       id: 2018,
-       indoor: 0,
-       longitude: '7.132',
-       altitude: '53.0' },
-    id: 5935220189,
-    timestamp: '2020-01-01 10:57:01',
-    sampling_rate: null,
-    sensor:
-     { sensor_type: { id: 9, name: 'DHT22', manufacturer: 'various' },
-       id: 4008,
-       pin: '7' } },
-  { sensordatavalues:
-     [ { value: '4.30', id: 12607161880, value_type: 'temperature' },
-       { value_type: 'humidity', value: '74.50', id: 12607161884 },
-       [length]: 2 ],
-    location:
-     { latitude: '50.98868222751',
-       id: 14536,
-       country: 'DE',
-       indoor: 0,
-       altitude: '50.4',
-       longitude: '7.03727155924',
-       exact_location: 1 },
-    id: 5935220187,
-    timestamp: '2020-01-01 10:57:01',
-    sampling_rate: null,
-    sensor:
-     { sensor_type: { manufacturer: 'various', id: 9, name: 'DHT22' },
-       id: 26988,
-       pin: '7' } },
-  { location:
-     { latitude: '50.98935082139',
-       exact_location: 1,
-       id: 20361,
-       country: 'BE',
-       altitude: '14.7',
-       indoor: 0,
-       longitude: '4.80760484934' },
-    id: 5935220172,
-    sensordatavalues:
-     [ { value_type: 'temperature', value: '5.30', id: 12607161847 },
-       { value: '78.90', id: 12607161848, value_type: 'humidity' },
-       [length]: 2 ],
-    sensor:
-     { sensor_type: { manufacturer: 'various', name: 'DHT22', id: 9 },
-       id: 34083,
-       pin: '7' },
-    timestamp: '2020-01-01 10:57:01',
-    sampling_rate: null },
+		location:
+		 { country: 'BE',
+			 id: 15177,
+			 indoor: 0,
+			 longitude: '4.412',
+			 altitude: '72.8',
+			 exact_location: 0,
+			 latitude: '50.844' },
+		sensordatavalues:
+		 [ { id: 12607161900, value: '29.57', value_type: 'P1' },
+			 { value: '16.87', id: 12607161901, value_type: 'P2' },
+			 [length]: 2 ],
+		sensor:
+		 { id: 27796,
+			 pin: '1',
+			 sensor_type: { manufacturer: 'Nova Fitness', name: 'SDS011', id: 14 } },
+		sampling_rate: null,
+		timestamp: '2020-01-01 10:57:01' },
+	{ location:
+		 { latitude: '51.842',
+			 exact_location: 0,
+			 id: 11774,
+			 country: 'NL',
+			 altitude: '30.9',
+			 indoor: 0,
+			 longitude: '5.858' },
+		id: 5935220194,
+		sensordatavalues:
+		 [ { value_type: 'temperature', id: 12607161896, value: '2.40' },
+			 { value_type: 'humidity', id: 12607161897, value: '99.90' },
+			 [length]: 2 ],
+		sensor:
+		 { id: 23210,
+			 pin: '7',
+			 sensor_type: { name: 'DHT22', id: 9, manufacturer: 'various' } },
+		timestamp: '2020-01-01 10:57:01',
+		sampling_rate: null },
+	{ sensordatavalues:
+		 [ { value_type: 'temperature', id: 12607161885, value: '10.30' },
+			 { value_type: 'humidity', value: '94.80', id: 12607161886 },
+			 [length]: 2 ],
+		location:
+		 { latitude: '50.796',
+			 exact_location: 0,
+			 country: 'DE',
+			 id: 2018,
+			 indoor: 0,
+			 longitude: '7.132',
+			 altitude: '53.0' },
+		id: 5935220189,
+		timestamp: '2020-01-01 10:57:01',
+		sampling_rate: null,
+		sensor:
+		 { sensor_type: { id: 9, name: 'DHT22', manufacturer: 'various' },
+			 id: 4008,
+			 pin: '7' } },
+	{ sensordatavalues:
+		 [ { value: '4.30', id: 12607161880, value_type: 'temperature' },
+			 { value_type: 'humidity', value: '74.50', id: 12607161884 },
+			 [length]: 2 ],
+		location:
+		 { latitude: '50.98868222751',
+			 id: 14536,
+			 country: 'DE',
+			 indoor: 0,
+			 altitude: '50.4',
+			 longitude: '7.03727155924',
+			 exact_location: 1 },
+		id: 5935220187,
+		timestamp: '2020-01-01 10:57:01',
+		sampling_rate: null,
+		sensor:
+		 { sensor_type: { manufacturer: 'various', id: 9, name: 'DHT22' },
+			 id: 26988,
+			 pin: '7' } },
+	{ location:
+		 { latitude: '50.98935082139',
+			 exact_location: 1,
+			 id: 20361,
+			 country: 'BE',
+			 altitude: '14.7',
+			 indoor: 0,
+			 longitude: '4.80760484934' },
+		id: 5935220172,
+		sensordatavalues:
+		 [ { value_type: 'temperature', value: '5.30', id: 12607161847 },
+			 { value: '78.90', id: 12607161848, value_type: 'humidity' },
+			 [length]: 2 ],
+		sensor:
+		 { sensor_type: { manufacturer: 'various', name: 'DHT22', id: 9 },
+			 id: 34083,
+			 pin: '7' },
+		timestamp: '2020-01-01 10:57:01',
+		sampling_rate: null },
 
 */
 
@@ -303,91 +311,91 @@ box: {lat1,lon1,lat2,lon2}, // all sensors in a 'box' with the given coordinates
 country: {countryCode}, // i.e. 'BE, DE, NL, ...'
 
 [ { id: 4767756759,
-    sampling_rate: null,
-    timestamp: '2019-09-03 20:13:54',
-    location:
-     { id: 13208,
-       latitude: '51.044',
-       longitude: '13.746',
-       altitude: '',
-       country: 'DE',
-       exact_location: 0,
-       indoor: 1 },
-    sensor:
-     { id: 36,
-       pin: '1',
-       sensor_type: { id: 14, name: 'SDS011', manufacturer: 'Nova Fitness' } },
-    sensordatavalues: [ { id: 10122094561, value: '40.80', value_type: 'humidity' } ] },
-  { id: 4767756688,
-    sampling_rate: null,
-    timestamp: '2019-09-03 20:13:53',
-    location:
-     { id: 13208,
-       latitude: '51.044',
-       longitude: '13.746',
-       altitude: '',
-       country: 'DE',
-       exact_location: 0,
-       indoor: 1 },
-    sensor:
-     { id: 36,
-       pin: '1',
-       sensor_type: { id: 14, name: 'SDS011', manufacturer: 'Nova Fitness' } },
-    sensordatavalues:
-     [ { id: 10122094401, value: '0.00', value_type: 'P1' },
-       { id: 10122094402, value: '0.00', value_type: 'P2' } ] },
-  { id: 4767756759,
-    sampling_rate: null,
-    timestamp: '2019-09-03 20:13:54',
-    location:
-     { id: 13208,
-       latitude: '51.044',
-       longitude: '13.746',
-       altitude: '',
-       country: 'DE',
-       exact_location: 0,
-       indoor: 1 },
-    sensor:
-     { id: 36,
-       pin: '1',
-       sensor_type: { id: 14, name: 'SDS011', manufacturer: 'Nova Fitness' } },
-    sensordatavalues:
-     [ { id: 10122094558, value: '27.00', value_type: 'temperature' } ] },
-  { id: 4767897022,
-    sampling_rate: null,
-    timestamp: '2019-09-03 20:33:25',
-    location:
-     { id: 16232,
-       latitude: '48.8',
-       longitude: '9.002',
-       altitude: '365.6',
-       country: 'DE',
-       exact_location: 0,
-       indoor: 0 },
-    sensor:
-     { id: 92,
-       pin: '1',
-       sensor_type: { id: 14, name: 'SDS011', manufacturer: 'Nova Fitness' } },
-    sensordatavalues:
-     [ { id: 10122391666, value: '4.10', value_type: 'P1' },
-       { id: 10122391670, value: '2.35', value_type: 'P2' } ] },
-  { id: 4767898407,
-    sampling_rate: null,
-    timestamp: '2019-09-03 20:33:36',
-    location:
-     { id: 16228,
-       latitude: '48.8',
-       longitude: '9.002',
-       altitude: '365.6',
-       country: 'DE',
-       exact_location: 0,
-       indoor: 0 },
-    sensor:
-     { id: 211,
-       pin: '1',
-       sensor_type: { id: 14, name: 'SDS011', manufacturer: 'Nova Fitness' } },
-    sensordatavalues:
-     [ { id: 10122394611, value: '4.95', value_type: 'P1' },
-       { id: 10122394612, value: '3.80', value_type: 'P2' } ] }
+		sampling_rate: null,
+		timestamp: '2019-09-03 20:13:54',
+		location:
+		 { id: 13208,
+			 latitude: '51.044',
+			 longitude: '13.746',
+			 altitude: '',
+			 country: 'DE',
+			 exact_location: 0,
+			 indoor: 1 },
+		sensor:
+		 { id: 36,
+			 pin: '1',
+			 sensor_type: { id: 14, name: 'SDS011', manufacturer: 'Nova Fitness' } },
+		sensordatavalues: [ { id: 10122094561, value: '40.80', value_type: 'humidity' } ] },
+	{ id: 4767756688,
+		sampling_rate: null,
+		timestamp: '2019-09-03 20:13:53',
+		location:
+		 { id: 13208,
+			 latitude: '51.044',
+			 longitude: '13.746',
+			 altitude: '',
+			 country: 'DE',
+			 exact_location: 0,
+			 indoor: 1 },
+		sensor:
+		 { id: 36,
+			 pin: '1',
+			 sensor_type: { id: 14, name: 'SDS011', manufacturer: 'Nova Fitness' } },
+		sensordatavalues:
+		 [ { id: 10122094401, value: '0.00', value_type: 'P1' },
+			 { id: 10122094402, value: '0.00', value_type: 'P2' } ] },
+	{ id: 4767756759,
+		sampling_rate: null,
+		timestamp: '2019-09-03 20:13:54',
+		location:
+		 { id: 13208,
+			 latitude: '51.044',
+			 longitude: '13.746',
+			 altitude: '',
+			 country: 'DE',
+			 exact_location: 0,
+			 indoor: 1 },
+		sensor:
+		 { id: 36,
+			 pin: '1',
+			 sensor_type: { id: 14, name: 'SDS011', manufacturer: 'Nova Fitness' } },
+		sensordatavalues:
+		 [ { id: 10122094558, value: '27.00', value_type: 'temperature' } ] },
+	{ id: 4767897022,
+		sampling_rate: null,
+		timestamp: '2019-09-03 20:33:25',
+		location:
+		 { id: 16232,
+			 latitude: '48.8',
+			 longitude: '9.002',
+			 altitude: '365.6',
+			 country: 'DE',
+			 exact_location: 0,
+			 indoor: 0 },
+		sensor:
+		 { id: 92,
+			 pin: '1',
+			 sensor_type: { id: 14, name: 'SDS011', manufacturer: 'Nova Fitness' } },
+		sensordatavalues:
+		 [ { id: 10122391666, value: '4.10', value_type: 'P1' },
+			 { id: 10122391670, value: '2.35', value_type: 'P2' } ] },
+	{ id: 4767898407,
+		sampling_rate: null,
+		timestamp: '2019-09-03 20:33:36',
+		location:
+		 { id: 16228,
+			 latitude: '48.8',
+			 longitude: '9.002',
+			 altitude: '365.6',
+			 country: 'DE',
+			 exact_location: 0,
+			 indoor: 0 },
+		sensor:
+		 { id: 211,
+			 pin: '1',
+			 sensor_type: { id: 14, name: 'SDS011', manufacturer: 'Nova Fitness' } },
+		sensordatavalues:
+		 [ { id: 10122394611, value: '4.95', value_type: 'P1' },
+			 { id: 10122394612, value: '3.80', value_type: 'P2' } ] }
 
 */
